ced6189e242bd2ec9bd0dd5785aafef4efe492d2
Matthew Vogee <mvogee@yahoo.com>
2020-11-20 17:17:48 -0800

    insertion sort algorithm

---
+++ insertion_sort.cpp
@@ -0,0 +1,17 @@
+#include "header.hpp"
+
+void swap (int& a, int& b) {
+  int c = a;
+  a = b;
+  b = c;
+
+}
+
+void insertionSort(int arr[], int length) {
+  int hold;
+  for (int i = 1; i < length; i++) {
+    for (int j = i; arr[j] < arr[j - 1] && j > 0; j--) {
+      swap(arr[j], arr[j - 1]);
+    }
+  }
+}
